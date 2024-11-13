fb01234c616725c09f4e5b73d01a520ad438c1d9
Hong Xu <hong@8hobbies.com>
2024-11-13 11:24:31 -0800

    Don't minify to offer better transparency
    Since we now use host_permissions for all URLs

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,13 @@ function generateManifest() {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
